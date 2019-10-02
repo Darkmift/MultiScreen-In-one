3ef90f56dc80b64de8607e5ea167bbd554f78c5b
dark_mift <darkmift@gmail.com>
2019-10-02 15:22:32 +0300

    bug fix ,key iteration in uuid4

--- main.js
+++ main.js
@@ -190,7 +190,7 @@ function uuidv4(stored_data) {
     let uuid = ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
         (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
     );
-    if (stored_data.includes(uuid)) {
+    if (Object.keys(stored_data).includes(uuid)) {
         uuidv4(stored_data);
     }
     return uuid;
